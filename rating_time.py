58aefecd75ec721054103f3f05ae3566147aef19
Nuo Liu <ivliu@Nuos-MacBook-Pro.local>
2019-04-07 13:48:12 -0700

    debug rating_time.py

--- rating_time.py
+++ rating_time.py
@@ -5,6 +5,9 @@ from constants import *
 import datetime
 import numpy as np
 def parseRatingOverTime(reviewfile,rolling_span=4):
+    """
+    parse the review csv and returns a pandas df with calculated ratings
+    """
     dateparse = lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
     #read in the review csv
     review = pd.read_csv(reviewfile, parse_dates = ['date'], date_parser = dateparse, encoding = "latin-1")
@@ -12,13 +15,14 @@ def parseRatingOverTime(reviewfile,rolling_span=4):
     group_biz = review.groupby(["business_id"], as_index=False).apply(lambda x: x.sort_values(['date'], ascending = True)).reset_index(drop=True)
     # group_biz['average_over_span'] = group_biz.groupby('business_id')['stars'].rolling(rolling_span).mean()
     average_over_span=group_biz.groupby('business_id')['stars'].rolling(rolling_span).mean()
+    group_biz["average_over_span"] = average_over_span.reset_index(level=0, drop=True)
     # group_biz["running_average"] = group_biz.groupby("business_id", as_index=False).apply(lambda x: x["stars"].expanding().mean())
     running_average= group_biz.groupby("business_id", as_index=False).apply(lambda x: x["stars"].expanding().mean())
-    group_biz=pd.merge(group_biz,average_over_span,on="review_id")
-    group_biz=pd.merge(group_biz,running_average,on="review_id")
+    group_biz["running_average"] = running_average.reset_index(level=0, drop=True)
     group_biz = group_biz[['business_id','date','average_over_span','running_average', 'review_id']]
-    reviewfile_name=reviewfile.split(".")[0]
-    group_biz.to_csv(DIRECTORY+reviewfile_name+"_ratingOverTime.csv", encoding="latin-1", index=False)
+    # reviewfile_name=reviewfile.split(".")[0]
+    # group_biz.to_csv(DIRECTORY+reviewfile_name+"_ratingOverTime.csv", encoding="latin-1", index=False)
+    return group_biz
     
 
 def main():
