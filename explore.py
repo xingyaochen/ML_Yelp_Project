21d16208b80d7dcd6c09065d2426fd1686620e9d
xchen-hmc <xchen@cs.hmc.edu>
2019-04-11 17:19:40 -0700

    reg

--- explore.py
+++ explore.py
@@ -11,6 +11,10 @@ import pandas as pd
 import datetime
 import numpy as np
 import matplotlib.pyplot as plt 
+from rating_time import *
+from sklearn.linear_model import LassoCV
+from sklearn.preprocessing import OneHotEncoder
+
 def extract_reviews_next(reader, review_file, colNames):
     review_df = pd.DataFrame(columns = colNames)
     first_time = 1
@@ -103,10 +107,12 @@ def construct_meta_features(biz_df):
 
     features_df = features_df[feature_colnames]
 
-    features_df_full = pd.concat([features_df, amb_df, goodMeal_df, bizPark_df])
+    features_df_full = pd.concat([features_df, amb_df, goodMeal_df, bizPark_df], axis = 1)
 
     features_df_clean = features_df_full.dropna(axis = 1, thresh = int(0.2*features_df_full.shape[0]))
+    return features_df_clean
 
+def factorize_features(features_df_clean):
     colnames_clean = list(features_df_clean)
     features_factor = pd.DataFrame(data= np.empty(features_df_clean.shape), columns = colnames_clean)
     feature_mapping = {}
@@ -132,6 +138,62 @@ def construct_meta_features(biz_df):
             features_factor[ft] = feat 
     return features_factor, feature_mapping
 
+def ohe_features(features_df_clean):
+    # line_contents_val = [d_attri[key] for key in column_names]
+    colnames_clean = list(features_df_clean)
+    ohe_list = []
+    for ft  in colnames_clean:
+        feat = features_df_clean[ft] 
+        # if isinstance(feat[0], str):
+        try:
+            feat = feat.str.replace("u'", "")
+            feat = feat.str.replace("'", "")
+            feat = [None if f == 'None' else f for f in feat]
+            feat_ohe = pd.get_dummies(feat)
+            feat_ohe.columns  = [ft+"_" + o for o in list(feat_ohe)]
+            ohe_list.append(feat_ohe)
+        except:
+            print(ft)
+            feat_ohe = pd.get_dummies(feat)
+            feat_ohe.columns  = [ft+"_" + o for o in list(feat_ohe)]
+            ohe_list.append(feat_ohe)
+            
+    featues_ohe =  pd.concat(ohe_list, axis = 1)
+    featues_ohe_names = [n for n in list(featues_ohe) if 'False' not in n]
+    return featues_ohe[featues_ohe_names]
 
-# line_contents_val = [d_attri[key] for key in column_names]
 
+
+biz = pd.read_csv(DIRECTORY+"filtered_business.csv", encoding= "utf-8")
+biz_id = biz['business_id']
+features_df_clean = construct_meta_features(biz)
+featues_ohe = ohe_features(features_df_clean)
+
+linked_featues_ohe = pd.concat([biz_id, featues_ohe], axis= 1)
+
+np.random.seed(1234)
+msk = np.random.rand(len(biz_id)) < 0.8
+train_biz = set(biz_id[msk])
+test_biz = set(biz_id[msk == False])
+
+linked_featues_ohe_train = linked_featues_ohe.loc[linked_featues_ohe['business_id'].isin(train_biz)]
+linked_featues_ohe_test = linked_featues_ohe.loc[linked_featues_ohe['business_id'].isin(test_biz)]
+
+reviewfile = DIRECTORY + "sorted_reviews.csv"
+reviewfile = DIRECTORY + "review.csv"
+review = parseRatingOverTime(reviewfile)
+
+train_data = linked_featues_ohe_train.merge(review, left_on = 'business_id', right_on = 'business_id')
+test_data = linked_featues_ohe_test.merge(review, left_on = 'business_id', right_on = 'business_id')
+
+# train_data = train_data.dropna(how = 'any')
+# test_data = test_data.dropna(how = 'any')
+
+X_train = train_data[list(linked_featues_ohe)[1:]]
+y_train = train_data['running_average']
+
+X_test = test_data[list(linked_featues_ohe)[1:]]
+y_test = test_data['running_average']
+
+reg = LassoCV(cv=8, random_state=0).fit(X_train, y_train)
+reg.score(X_test, y_test)
