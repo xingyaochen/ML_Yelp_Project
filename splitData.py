92e23ac51980d1c001c5ec7eeb70624185bb3692
elenavr <eromero@hmc.edu>
2019-04-12 15:16:38 -0700

    added review splitting to splitData.py

--- splitData.py
+++ splitData.py
@@ -8,7 +8,13 @@ import numpy as np
 bizfile = DIRECTORY + "filtered_business.csv"
 bizData = pd.read_csv(bizfile, encoding= "latin-1")
 
-def splitDat(data, percTest):
-    """Splits data with specified test set percentage"""
-    train, test= train_test_split(data, test_size= percTest)
-    return train, test
+def splitBusinessID(data, percTrain):
+    """Splits data with specified training set percentage 
+    returns train and test dataframes"""
+    # train, test= train_test_split(data, test_size= percTest, random_state = 1234)
+    # return train, test
+    np.random.seed(1234)
+    msk = np.random.rand(len(biz_id)) < percTrain
+    train_biz = set(biz_id[msk])
+    test_biz = set(biz_id[msk == False])
+    return train_biz, test_biz
