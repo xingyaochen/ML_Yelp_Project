4093674c0f7e2dcae47337fecdc506eb8d0c8d39
elenavr <eromero@hmc.edu>
2019-04-19 13:19:05 -0700

    started working on setting up crossvalidation

---
+++ crossval.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt 
+import random
+from rating_time import *
+from explore import *
+
+#hold out percentage
+#train on %50 of training data and then validate on following 10%
+# choose random int between 0 and 40 train on 50% of all reviews starting from that point
+# then validate on 10% of reviews directly after
+
+#n*36 pandas frame metadata on same row as review
+#y = n cumulative review ratings
+def singleBizInterval(startperc,singleBiz):
+    #get number of reviews for specific business maybe use num rows in single biz
+    numreviews = 
+    #get starting and ending rows for train and validation reviews
+    trainstart = numreviews*startperc
+    trainend = (numreviews * 0.5) + trainstart
+    valstart = trainend + 1 
+    valend = valstart + (numreviews * 0.1)
+    #index into df and just get rows between review start and review end
+    return trainx, trainy, valx, valy
+
+#get smaller data frame for each business
+
+
+
+
+# #get reviews for all businesses
+# reviewfile = DIRECTORY + "review.csv"
+# #parse the ratings
+
+# # print(reviews)
+
+# #chunk reviews by business id
+# dateparse = lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
+# review = pd.read_csv(DIRECTORY + "review.csv",  parse_dates = ['date'], date_parser = dateparse, encoding = "latin-1")
+# review_sorted = review.sort_values(by = ['business_id', 'date'])
+# review_sorted.to_csv(DIRECTORY + "sorted_reviews.csv")
+# rateTime = parseRatingOverTime(DIRECTORY + "sorted_reviews.csv")
+
+# print (rateTime)
