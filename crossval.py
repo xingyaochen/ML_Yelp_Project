6aaa8a93acd93c2b3cdb3c4e0754f99b141764d8
elenavr <eromero@hmc.edu>
2019-04-19 21:56:35 -0700

    worked on getting crossvalidation folds

--- crossval.py
+++ crossval.py
@@ -10,44 +10,40 @@ from explore import *
 #train on %50 of training data and then validate on following 10%
 # choose random int between 0 and 40 train on 50% of all reviews starting from that point
 # then validate on 10% of reviews directly after
+currdata = pd.read_csv(DIRECTORY+"training.csv", encoding= "utf-8")
 
-#n*36 pandas frame metadata on same row as review
-#y = n cumulative review ratings
 def singleBizInterval(startperc,singleBiz):
-    #get number of reviews for specific business maybe use num rows in single biz
-    numreviews = 
+    """Indexes into dataframe of reviews for a single business (singleBiz).
+    Gets 50% of all reviews starting at startperc and then puts the following 10% into
+    a validation set"""
+    #get number of reviews for specific business
+    numreviews = singleBiz.shape[0]
     #get starting and ending rows for train and validation reviews
-    trainstart = numreviews*startperc
-    trainend = (numreviews * 0.5) + trainstart
-    valstart = trainend + 1 
-    valend = valstart + (numreviews * 0.1)
+    trainstart = int(numreviews*startperc)
+    trainend = int((numreviews * 0.5) + trainstart)
+    valstart = trainend
+    valend = int(valstart + (numreviews * 0.1))
     #index into df and just get rows between review start and review end
-    return trainx, trainy, valx, valy
+    trainData = singleBiz.iloc[trainstart:trainend]
+    valData = singleBiz.iloc[valstart:valend]
+    return trainData, valData
 
-def getBizIntervals(startperc):
-    #need to add in dataframe here
-    businesses = np.unique(['business_id'])
-    for biz in businesses:
-        currentdf = df.loc[df['business_id'] == biz]
-        #call single biz interval here
-    #somehow concatenate all of these into one dataframe again
-    
-    
-#get smaller data frame for each business
-
-
-
-# #get reviews for all businesses
-# reviewfile = DIRECTORY + "review.csv"
-# #parse the ratings
-
-# # print(reviews)
-
-# #chunk reviews by business id
-# dateparse = lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
-# review = pd.read_csv(DIRECTORY + "review.csv",  parse_dates = ['date'], date_parser = dateparse, encoding = "latin-1")
-# review_sorted = review.sort_values(by = ['business_id', 'date'])
-# review_sorted.to_csv(DIRECTORY + "sorted_reviews.csv")
-# rateTime = parseRatingOverTime(DIRECTORY + "sorted_reviews.csv")
-
-# print (rateTime)
+def getBizIntervals(startperc, train_data):
+    """For each business in train_data, grabs 50% of that business' reviews
+    starting at startperc and then grabs the following 10% to be used as validation"""
+    #get list of businesses
+    all_biz = np.unique(train_data['business_id'])
+    #for each business separate the correct number of reviews
+    validationdf = []
+    traindf = []
+    for biz in all_biz:
+        currentdf = train_data[train_data['business_id'] == biz]
+        singTrain, singVal = singleBizInterval(startperc, currentdf)
+        validationdf.append(singVal)
+        traindf.append(singTrain)
+    #put all the reviews back into dataframes
+    validationdf = pd.concat(validationdf, axis =0)
+    traindf = pd.concat(traindf, axis=0)
+    print(validationdf)
+    print(traindf)
+    return validationdf, traindf
