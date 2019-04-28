63b09db65692c98ef44a473c6c0e17f0be555c2f
Nuo Liu <ivliu@WL-214-184.WPA.Claremont.Edu>
2019-04-27 19:58:28 -0700

    added CV for linear SVM

--- sentiment_analysis_reviews.py
+++ sentiment_analysis_reviews.py
@@ -22,6 +22,49 @@ from sklearn.svm import SVC
 from sklearn.feature_extraction.text import TfidfVectorizer
 from crossval import *
 
+def performance(y_true, y_pred, metric="accuracy") :
+    """
+    Calculates the performance metric based on the agreement between the 
+    true labels and the predicted labels.
+    
+    Parameters
+    --------------------
+        y_true -- numpy array of shape (n,), known labels
+        y_pred -- numpy array of shape (n,), (continuous-valued) predictions
+        metric -- string, option used to select the performance measure
+                  options: 'accuracy', 'f1_score', 'auroc', 'precision',
+                           'sensitivity', 'specificity'        
+    
+    Returns
+    --------------------
+        score  -- float, performance score
+    """
+    # map continuous-valued predictions to binary labels
+    y_label = np.sign(y_pred)
+    y_label[y_label==0] = 1 # map points of hyperplane to +1
+    
+    ### ========== TODO : START ========== ###
+    # part 2a: compute classifier performance
+    if metric == 'accuracy':
+        return metrics.accuracy_score(y_true, y_label)
+    if metric == 'f1_score':
+        return metrics.f1_score(y_true, y_label)
+    if metric == 'auroc':
+        return metrics.roc_auc_score(y_true, y_pred, average='micro')
+    if metric == 'precision':
+        return metrics.precision_score(y_true, y_label)
+    if metric == 'recall' or metric == 'sensitivity':
+        return metrics.recall_score(y_true, y_label)
+    if metric == 'specificity':
+        confusionMatrix = metrics.confusion_matrix(y_true, y_label, labels = [1, -1])
+        falsePos = confusionMatrix[1][0]
+        falseNeg = confusionMatrix[0][1]
+        truePos = confusionMatrix[0][0]  
+        trueNeg = confusionMatrix[1][1]
+        if (trueNeg+falsePos) > 0:
+            return float(trueNeg)/(trueNeg+falsePos)
+        return 0
+
 def addSentimentToFeature(business_df,business_sentiment):
     """
     params
@@ -64,7 +107,7 @@ def calculateAveragePolarity(business_ids,predicted_polarity):
     return business_sentiment
 
 
-def parseReviewDF(df,cumulative_rating=False):
+def parseReviewDF(df,cumulative_rating=True):
     """
     parse the review pandas df and return a new pandas df with only the text and review_Id
 
@@ -84,7 +127,7 @@ def parseReviewDF(df,cumulative_rating=False):
         review_text.rename(columns={'average_over_span':'rating'}, inplace=True)
     return review_text
 
-def preprocess(review_text_,binary_rating=True,Tfidf=False):
+def preprocess(review_text_,n_gram=1,binary_rating=True,Tfidf=False):
     """
     tokenize each review text into a list of words also add a col for length of the tokenized text.
     Set binary_rating to true if we want to deal with 0(bad) and 1(good) as labels instead of 1-5 scaled ratings
@@ -105,8 +148,15 @@ def preprocess(review_text_,binary_rating=True,Tfidf=False):
         if Tfidf==False:
             #only change text to BOG if Tfidf is false
             tokenizeWords=word_tokenize(row_series['text'])
-            tokenizeWords=removeStopWords(tokenizeWords)
-            tokenizeWords=convertToStem(tokenizeWords)
+            tokenizeWords=preprocess_Review(tokenizeWords)
+            if n_gram==1: pass
+            elif n_gram==2:
+                tokenizeWords=bigram(tokenizeWords)
+            elif n_gram==3:
+                tokenizeWords=trigram(tokenizeWords)
+            else:
+                raise Exception("ONLY support n_gram for n= 1, 2, 3!!!")
+            
             review_text.at[index_label , 'text'] = tokenizeWords
 
         if binary_rating:
@@ -117,25 +167,23 @@ def preprocess(review_text_,binary_rating=True,Tfidf=False):
 
     return review_text
 
-def removeStopWords(tokenized_sent):
+def preprocess_Review(tokenized_sent):
     stop_words=set(stopwords.words("english"))
+    ps = PorterStemmer()
     filtered_sent=[]
     for w in tokenized_sent:
+        w=w.lower()
         if w not in stop_words:
-            filtered_sent.append(w)
+            word=ps.stem(w)
+            filtered_sent.append(word)
     return filtered_sent
 
-def convertToStem(tokenized_sent):
-    ps = PorterStemmer()
-    for word in tokenized_sent:
-        word=ps.stem(word)
-    return tokenized_sent
+def bigram(tokenized_sent):
+    return list(nltk.bigrams(tokenized_sent))
 
-# def frequencyDistribution(review_text):
-#     """
-#     Input: pandas df with tokenized text in the "tokenizeWords" column
-#     Plots a frequency distribution and returns the frequency distribution
-#     """
+def trigram(tokenized_sent):
+    return list(nltk.trigrams(tokenized_sent))
+
 
 def extract_dictionary(allwords) :
     """
@@ -170,16 +218,15 @@ def extract_feature_vectors(tokenizeWords, word_list) :
     --------------------
         feature_matrix -- numpy array of shape (n,d)
                           boolean (0,1) array indicating word presence in a string
-                            n is the number of non-blank lines in the text file
+                            n is the number of reviews
                             d is the number of unique words in the text file
     """
  
     num_lines = tokenizeWords.count()
     num_words = len(word_list)
-    feature_matrix = np.zeros((num_lines, num_words))
+    feature_matrix = np.zeros(num_words)
  
     for i, words in enumerate(tokenizeWords.tolist()):
-        #loop through each word in the tweet
         for word in words:
             #find the index in the dictionary
             index=word_list[word]
@@ -249,24 +296,9 @@ def analysis(review_text,Tfidf=False):
     print("linear SVC Accuracy: ",acc)
     print("linear SVC f1: ",f1)
     
-    # rbf=SVC(C=32,gamma=0.0078125,kernel='rbf')
-    # rbf.fit(X_train,y_train)
-    # pred_rbf=rbf.decision_function(X_train)
-    # majority=DummyClassifier(strategy="most_frequent")
-    # majority.fit(X_train,y_train)
-    # pred_majority=majority.predict(X_train)
+ 
 
-
-
-
-    # #plot the distribution in the dataset
-    # Sentiment_count=review_text.groupby('rating').count()
-    # plt.bar(Sentiment_count.index.values, Sentiment_count['text'])
-    # plt.xlabel('Review Sentiments')
-    # plt.ylabel('Number of Review')
-    # plt.show()
-
-def cv_performance(metrics=["accuracy"]) :
+def cross_validation(clf,metrics=["accuracy","f1_score"],Tfidf=False) :
     """
     Splits the data, X and y, into k-folds and runs k-fold cross-validation.
     Trains classifier on k-1 folds and tests on the remaining fold.
@@ -292,103 +324,140 @@ def cv_performance(metrics=["accuracy"]) :
     reviewdf = pd.read_csv(reviewfile, encoding = "latin-1")
 
     #do preprocessing here !!!!
+    reviewdf=parseReviewDF(reviewdf)
+    #using BoW
+ 
+    if Tfidf==False:
+        reviewdf=preprocess(reviewdf,Tfidf=False)
+        texts=reviewdf["text"]
+        #USING BAD OF WORDS
+        dictionary = extract_dictionary(texts)
+        bow=extract_feature_vectors(texts,dictionary)
+        reviewdf.update({"text":bow})
+    if Tfidf==True:
+        reviewdf=preprocess(reviewdf,Tfidf=True)
+        tf=TfidfVectorizer()
+        X= tf.fit_transform(reviewdf["text"])
+        reviewdf.update({"text":X})
+    print(reviewdf["rating"].value_counts())
+
 
     #make a nested dictionary of a reviews, first key is column you want and second is review id
     reviewdict = reviewdf.set_index('review_id').to_dict()
-    # print(reviewdict['text']['lgpSS6UsKYIvnQaw8JwHlQ'])
-    # print(reviewdict['running_average']['lgpSS6UsKYIvnQaw8JwHlQ'])
+    return cv_performance(clf,reviewdict,metrics)
+    
+    
+    #----------------------------------------------------------
+    #CROSS VALIDATION BELOW
+def cv_performance(clf,reviewdict,metrics=["accuracy","f1_score"]) :
 
     crossValFile = DIRECTORY + "crossVal.csv"
     #get crossvalidation data
     crossValdf = pd.read_csv(crossValFile, encoding = "latin-1")
-
-    #loop through sets
+    m = len(metrics)
+    k=len(np.unique(crossValdf['foldNum']))
+    scores = np.empty((m, k))
+    #For each fold
     for currfold in np.unique(crossValdf['foldNum']):
+        #get its training set and validation set
         train_data = []
         validation_data = []
 
-        #get review_ids from each set
-        print('training and validation sizes for current sets are')
+        #get the segments of data
         train_cv, validate_cv = get_cv_fold(crossValdf, currfold)
-        review_id_train = train_cv['review_id']
-        review_id_validate = validate_cv['review_id']
+
 
         #match review_ids and put them in correct dataframe
         for index, row in train_cv.iterrows():
             curr_id = row['review_id']
-            train_data.append([reviewdict['text'][curr_id],reviewdict['running_average'][curr_id]])
+            train_data.append([reviewdict['text'][curr_id],reviewdict['rating'][curr_id]])
         for index, row in validate_cv.iterrows():
             curr_id = row['review_id']
-            validation_data.append([reviewdict['text'][curr_id],reviewdict['running_average'][curr_id]])
-        traindf = pd.DataFrame(train_data, columns = ['text', 'running_average'])
-        validatedf = pd.DataFrame(validation_data, columns = ['text', 'running_average'])
-        traindf.to_csv(DIRECTORY+"traindf"+str(currfold)+".csv", encoding="latin-1", index=False)
-        validatedf.to_csv(DIRECTORY+"validatedf"+str(currfold)+".csv", encoding="latin-1", index=False)
+            validation_data.append([reviewdict['text'][curr_id],reviewdict['rating'][curr_id]])
+        traindf = pd.DataFrame(train_data, columns = ['text', 'rating'])
+        validatedf = pd.DataFrame(validation_data, columns = ['text', 'rating'])
+        # traindf.to_csv(DIRECTORY+"traindf"+str(currfold)+".csv", encoding="latin-1", index=False)
+        # validatedf.to_csv(DIRECTORY+"validatedf"+str(currfold)+".csv", encoding="latin-1", index=False)
+        y_train=traindf["rating"].values
+        X_train=traindf["text"].values
+        y_val=validatedf["rating"].values
+        X_val=validatedf["text"].values
+
+        clf = model.fit(X_train,y_train)
+        y_pred= clf.predict(X_val)
+        for m, metric in enumerate(metrics):
+            score = performance(y_val, y_pred, metric)
+            scores[m,k] = score
+
+    return scores.mean(axis=1)
         
 
 
-    #leftovers from function I repurposed from
-    # for k, (train, test) in enumerate(kf.split(X, y)) :
-    #     X_train, X_test, y_train, y_test = X[train], X[test], y[train], y[test]
-    #     clf.fit(X_train, y_train)
-    #     # use SVC.decision_function to make ``continuous-valued'' predictions
-    #     y_pred = clf.decision_function(X_test)
-    #     for m, metric in enumerate(metrics) :
-    #         score = performance(y_test, y_pred, metric)
-    #         scores[m,k] = score
-            
-    # return scores.mean(axis=1) # average across columns
+ def lineplot(x, y, label):
+    """
+    Make a line plot.
+    
+    Parameters
+    --------------------
+        x            -- list of doubles, x values
+        y            -- list of doubles, y values
+        label        -- string, label for legend
+    """
+    
+    xx = range(len(x))
+    plt.plot(xx, y, linestyle='-', linewidth=2, label=label)
+    plt.xticks(xx, x)
 
-# def select_param_linear(X, y, kf, metrics=["accuracy"], plot=True) :
-#     """
-#     Sweeps different settings for the hyperparameter of a linear-kernel SVM,
-#     calculating the k-fold CV performance for each setting and metric,
-#     then selects the hyperparameter that maximizes the average performance for each metric.
+def select_param_linear(X, y,metrics=["accuracy","f1_score"], plot=True,Tfidf=False) :
+    """
+    Sweeps different settings for the hyperparameter of a linear-kernel SVM,
+    calculating the k-fold CV performance for each setting and metric,
+    then selects the hyperparameter that maximizes the average performance for each metric.
     
-#     Parameters
-#     --------------------
-#         X       -- numpy array of shape (n,d), feature vectors
-#                      n = number of examples
-#                      d = number of features
-#         y       -- numpy array of shape (n,), binary labels {1,-1}
-#         kf      -- model_selection.KFold or model_selection.StratifiedKFold
-#         metrics -- list of m strings, metrics
-#         plot    -- boolean, make a plot
+    Parameters
+    --------------------
+        X       -- numpy array of shape (n,d), feature vectors
+                     n = number of examples
+                     d = number of features
+        y       -- numpy array of shape (n,), binary labels {1,-1}
+        kf      -- model_selection.KFold or model_selection.StratifiedKFold
+        metrics -- list of m strings, metrics
+        plot    -- boolean, make a plot
     
-#     Returns
-#     --------------------
-#         params  -- list of m floats, optimal hyperparameter C for each metric
-#     """
+    Returns
+    --------------------
+        params  -- list of m floats, optimal hyperparameter C for each metric
+    """
     
-#     C_range = 10.0 ** np.arange(-3, 3)
-#     scores = np.empty((len(metrics), len(C_range)))
+    C_range = 10.0 ** np.arange(-3, 3)
+    scores = np.empty((len(metrics), len(C_range)))
     
-#     ### ========== TODO : START ========== ###
-#     # part 3b: for each metric, select optimal hyperparameter using cross-validation
-#     for j, c in enumerate(C_range):
-#         model_svc = LinearSVC(dual=False)
-#         # compute CV scores using cv_performance(...)
-#         scores[:,j] = cv_performance(model_svc, X, y, kf, metrics)
+    ### ========== TODO : START ========== ###
+    # part 3b: for each metric, select optimal hyperparameter using cross-validation
+    for j, c in enumerate(C_range):
+        model_svc = LinearSVC(dual=False,C=c)
+        # compute CV scores using cv_performance(...)
+        scores[:,j] = cross_validation(model_svc,metrics,Tfidf=False)
 
-#     # get best hyperparameters
-#     best_params_ind = np.argmax(scores,  axis=1)    # dummy, okay to change
-#     best_params = C_range[best_params_ind]
-#     ### ========== TODO : END ========== ###
+    # get best hyperparameters
+    best_params_ind = np.argmax(scores,  axis=1)    # dummy, okay to change
+    best_params = C_range[best_params_ind]
+    ### ========== TODO : END ========== ###
     
-#     # plot
-#     if plot:
-#         plt.figure()
-#         ax = plt.gca()
-#         ax.set_ylim(0, 1)
-#         ax.set_xlabel("C")
-#         ax.set_ylabel("score")
-#         for m, metric in enumerate(metrics) :
-#             lineplot(C_range, scores[m,:], metric)
-#         plt.legend()
-#         plt.savefig("linear_param_select.png")
-#         plt.close()
+    # plot
+    if plot:
+        plt.figure()
+        ax = plt.gca()
+        ax.set_ylim(0, 1)
+        ax.set_xlabel("C")
+        ax.set_ylabel("score")
+        for m, metric in enumerate(metrics) :
+            lineplot(C_range, scores[m,:], metric)
+        plt.legend()
+        plt.savefig("linear_param_select.png")
+        plt.close()
     
-#     return best_params
+    return best_params
  
 
 
