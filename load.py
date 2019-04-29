c5b2b63ad265e8de61fd9340e405d448d06858d8
xchen-hmc <xchen@cs.hmc.edu>
2019-04-29 02:31:51 -0700

    kms2

--- load.py
+++ load.py
@@ -53,7 +53,7 @@ def read_and_write_csv(json_file_path, csv_file_path, filterD = {}):
 
 # # filterD = {'city': {"Las Vegas"}}
 def main():
-    filterD = {'state': {'NV'}, 'review_count': 1000, 'categories': {'Food', 'Restaurants','Bars', 'Breakfast', 'Lunch', 'Dinner', 'Eatertainment'}}
+    filterD = {'city': {'Las Vegas'}, 'review_count': 3500, 'categories': {'Food', 'Restaurants','Bars', 'Breakfast', 'Lunch', 'Dinner', 'Eatertainment'}}
 
     json_file_path = DIRECTORY + 'business.json'
     csv_file_path = DIRECTORY + 'business.csv'
@@ -62,7 +62,7 @@ def main():
     city, counts = np.unique(business['city'], return_counts=True)
 
     # change this value to change the threshold value of number of businesses in the area
-    threshold = 100
+    threshold = 0
 
     # filter by city and number of businesses in that city
     cities_alot = city[counts > threshold]
@@ -86,4 +86,36 @@ def main():
     # review_sorted.to_csv(DIRECTORY + "sorted_reviews.csv")
 
 if __name__ == "__main__":
-    main()
+    pass
+    # main()
+
+
+# filterD = {'city': {'Las Vegas'}, 'review_count': 3500, 'categories': {'Food', 'Restaurants','Bars', 'Breakfast', 'Lunch', 'Dinner', 'Eatertainment'}}
+
+# json_file_path = DIRECTORY + 'business.json'
+# csv_file_path = DIRECTORY + 'business.csv'
+# read_and_write_csv(json_file_path, csv_file_path, filterD)
+# business = pd.read_csv (DIRECTORY + "business.csv", encoding = "latin-1")
+# city, counts = np.unique(business['city'], return_counts=True)
+
+# # change this value to change the threshold value of number of businesses in the area
+# threshold = 0
+
+# # filter by city and number of businesses in that city
+# cities_alot = city[counts > threshold]
+# business.set_index('city', inplace=True)
+# business_filtered = business.loc[cities_alot]
+# business_filtered.to_csv(DIRECTORY + "filtered_business.csv")
+
+# # make review csv
+# # # change the "business.csv" to "filtered_business.csv" if the location filtering is desired
+# biz = pd.read_csv(DIRECTORY+"filtered_business.csv", encoding= "latin-1")
+# biz_id = biz['business_id']
+# filterD_reviews = {'business_id': set(biz_id)}
+# json_review_path = DIRECTORY + 'review.json'
+# csv_review_path = DIRECTORY + 'review.csv'
+# read_and_write_csv(json_review_path, csv_review_path, filterD_reviews)
+# dateparse = lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
+
+# review = pd.read_csv(DIRECTORY + "review.csv",  parse_dates = ['date'], date_parser = dateparse, encoding = "latin-1")
+# # print(review.shape)
